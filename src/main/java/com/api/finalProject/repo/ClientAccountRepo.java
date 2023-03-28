b476b4f0208316776b92cb20c340ba26cec5df5f
Oleg <kovalyovop@yandex.ru>
2023-03-28 21:44:20 +0300

    28/03/2023

---
+++ src/main/java/com/api/finalProject/repo/ClientAccountRepo.java
@@ -0,0 +1,8 @@
+package com.api.finalProject.repo;
+
+import com.api.finalProject.entity.ClientAccountEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientAccountRepo extends CrudRepository<ClientAccountEntity, Long> {
+
+}
